949192c1163928630df86d4c84ed3c7ac6389d24
justin connors <justinconnors@hotmail.ca>
2023-04-10 22:12:14 -0300

    got connection working, started work on script.js, got a menu in there and 1 function so far

---
+++ Assets/JS/script.js
@@ -0,0 +1,56 @@
+const inquirer = require('inquirer');
+const sequelize = require('../config/connection');
+
+const menu = async () => {
+    const choices = [
+        { name: 'View all departments', value: 'viewDepartments' },
+        { name: 'View all roles', value: 'viewRoles' },
+        { name: 'View all employees', value: 'viewEmployees' },
+        { name: 'Add a department', value: 'addDepartment' },
+        { name: 'Add a role', value: 'addRole' },
+        { name: 'Add an employee', value: 'addEmployee' },
+        { name: 'Update an employee role', value: 'updateEmployeeRole' },
+        { name: 'Exit', value: 'exit' },
+    ];
+
+    const { choice } = await inquirer.prompt({
+        type: 'list',
+        name: 'choice',
+        message: 'Please select an option:',
+        choices,
+    });
+    
+    switch (choice) {
+        case 'viewDepartments':
+            viewDepartments();
+            break;
+        case 'viewRoles':
+            break;
+        case 'viewEmployees':
+            break;
+        case 'addDepartment':
+            break;
+        case 'addRole':
+            break;
+        case 'addEmployee':
+            break;
+        case 'updateEmployeeRole':
+            break;
+        case 'exit':
+            console.log('Goodbye!');
+            break;
+        default:
+            console.log('Invalid choice!');
+            menu();
+            break;
+      }
+};
+
+//View departments
+const viewDepartments = async () => {
+    const [rows] = await sequelize.query('SELECT * FROM departments');
+    console.table(rows);
+    menu();
+}
+
+menu();
